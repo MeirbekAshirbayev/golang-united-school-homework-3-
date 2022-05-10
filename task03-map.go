5344104582e66b366163362920bd62dc31a706a2
Meirbek <53935454+MeirbekAshirbayev@users.noreply.github.com>
2022-05-10 20:31:56 +0600

    Add files via upload

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,20 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
-	return
+	var (
+		num []int
+	)
+
+	for k, _ := range input {
+		num = append(num, k)
+	}
+	sort.Ints(num)
+
+	for _, v := range num {
+		result = append(result, input[v])
+	}
+	return result
 }
